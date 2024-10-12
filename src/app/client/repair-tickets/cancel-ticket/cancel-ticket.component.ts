34dbccfd76db60b39d9606d119578936bf490401
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-12 20:24:47 +0100

    fix functionalities

---
+++ src/app/client/repair-tickets/cancel-ticket/cancel-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './cancel-ticket.component.html',
+  styleUrl: './cancel-ticket.component.css'
+})
+export class CancelTicketComponent {
+
+}
