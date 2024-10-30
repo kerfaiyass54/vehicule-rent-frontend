e006563814a283a79f72e7dc21dd5bdbebebefe6
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-30 14:37:52 +0100

    Added location management UI

---
+++ src/app/admin/location-management/location-details/location-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-details.component.html',
+  styleUrl: './location-details.component.css'
+})
+export class LocationDetailsComponent {
+
+}
