0e68932bad6fd0ccda4ea6bcf68fcb4d27040e79
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-30 14:38:44 +0100

    Added repair management UI

---
+++ src/app/admin/repair-management/update-repair/update-repair.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-repair',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-repair.component.html',
+  styleUrl: './update-repair.component.css'
+})
+export class UpdateRepairComponent {
+
+}
