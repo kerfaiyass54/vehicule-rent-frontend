ed51f95df9373b03c71f0fac5721f985e4e91e23
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-31 16:10:34 +0100

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {ClientComponent} from "./client/client.component";
+import {AdminComponent} from "./admin/admin.component";
+import {SupplierComponent} from "./supplier/supplier.component";
+import {RepairComponent} from "./repair/repair.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'client',
+    component:ClientComponent,
+    title:"Client Dashboard"
+  },{
+    path:'admin',
+    component:AdminComponent,
+    title:'Admin Dashboard'
+  },{
+    path:'supplier',
+    component:SupplierComponent,
+    title:'Supplier Dashboard'
+  },{
+    path:'repair',
+    component:RepairComponent,
+    title:'Repair Dashboard'
+  }
+];
