1cc8bb51f53edf8d05d51bc87b84c2fd0101061e
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-05 10:31:50 +0100

    Added categories management to suppliers

---
+++ src/app/supplier/management-categories/management-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './management-categories.component.html',
+  styleUrl: './management-categories.component.css'
+})
+export class ManagementCategoriesComponent {
+
+}
