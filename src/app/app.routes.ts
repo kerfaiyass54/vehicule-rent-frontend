0f489a34b93efc58a4e1def991e8052a8974f87e
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-05 10:36:02 +0100

    Added routers to client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,13 +8,36 @@ import {AdminUpdateComponent} from "./admin/admin-update/admin-update.component"
 import {LocationManagementComponent} from "./admin/location-management/location-management.component";
 import {RepairManagementComponent} from "./admin/repair-management/repair-management.component";
 import {SupplierManagementComponent} from "./admin/supplier-management/supplier-management.component";
+import {ClientDetailsComponent} from "./client/client-details/client-details.component";
+import {RepairTicketsComponent} from "./client/repair-tickets/repair-tickets.component";
+import {SubscriptionsManagementComponent} from "./client/subscriptions-management/subscriptions-management.component";
+import {VehiculesBuyingsComponent} from "./client/vehicules-buyings/vehicules-buyings.component";
 
 export const routes: Routes = [
   {
     path:'client',
     component:ClientComponent,
-    title:"Client Dashboard"
-  },{
+    title:"Client Dashboard",
+    children:[
+      {
+        path:'details',
+        component:ClientDetailsComponent
+      },
+      {
+        path:'tickets',
+        component: RepairTicketsComponent
+      },
+      {
+        path:'subscriptions',
+        component: SubscriptionsManagementComponent
+      },
+      {
+        path:'buyings',
+        component: VehiculesBuyingsComponent
+      }
+    ]
+  },
+  {
     path:'admin',
     component:AdminComponent,
     title:'Admin Dashboard',
