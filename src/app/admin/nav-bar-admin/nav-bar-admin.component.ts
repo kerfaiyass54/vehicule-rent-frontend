3ebdabc965cd30b6a9540f0ce6188ac8e1c1db6c
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-05 11:22:18 +0100

    Added routers to admin navigation

--- src/app/admin/nav-bar-admin/nav-bar-admin.component.ts
+++ src/app/admin/nav-bar-admin/nav-bar-admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './nav-bar-admin.component.html',
   styleUrl: './nav-bar-admin.component.css'
 })
