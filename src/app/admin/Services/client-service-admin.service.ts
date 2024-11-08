407711fb467024020733cf5614d3d8797660ab1c
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-08 16:21:24 +0100

    Created services for admin-clients

--- src/app/admin/Services/client-service-admin.service.ts
+++ src/app/admin/Services/client-service-admin.service.ts
@@ -7,11 +7,37 @@ import {Client} from "../../models/client";
 })
 export class ClientServiceAdminService {
   baseUrl ="http://localhost:8080/";
-    constructor(private http:HttpClient) {
+    constructor(private http:HttpClient) {}
 
+  createClient(client: Client, ){
+    return  this.http.post<any>(this.baseUrl + "client/adding",client);
   }
 
-  createClient(client: Client){
-    return  this.http.post<any>(this.baseUrl + "client/adding",client);
+  getAllClients(){
+      return this.http.get<any>(this.baseUrl + "admin/clients");
+  }
+
+  deleteClient(id: any){
+      return this.http.delete(this.baseUrl + "client/" + id);
+  }
+
+  getClient(name: any){
+      return this.http.get(this.baseUrl + "client/" + name);
+  }
+
+  updateClient(client: Client){
+      return this.http.put(this.baseUrl + "client/update", client);
+  }
+
+  isClientExist(name: String){
+      return this.http.get(this.baseUrl + "client/exist/" + name );
+  }
+
+  changeLocation(client: String, location: String){
+    return this.http.get(this.baseUrl + "client/location/" + client + "?newLocation=" + location );
+  }
+
+  isCinExists(cin: any){
+    return this.http.get(this.baseUrl + "client/cin/exist/" + cin);
   }
 }
