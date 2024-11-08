959fe8a232b81878ed405c2dd9ad8f6324a1aae1
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-08 16:30:19 +0100

    Created services for admin-locations

--- src/app/admin/Services/location-service-admin.service.ts
+++ src/app/admin/Services/location-service-admin.service.ts
@@ -6,7 +6,37 @@ import {HttpClient} from "@angular/common/http";
 })
 export class LocationServiceAdminService {
   baseUrl ="http://localhost:8080/";
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
+  getRepairs(name: String){
+    return this.http.get(this.baseUrl + "location/repairs");
+  }
+
+  getSuppliers(name: String){
+    return this.http.get(this.baseUrl + "location/suppliers");
+  }
+
+  getClients(name: String){
+    return this.http.get(this.baseUrl + "location/clients");
+  }
+
+  addLocation(location: Location){
+    return this.http.post(this.baseUrl + "location/add", location);
+  }
+
+  deleteLocation(name: String){
+    return this.http.get(this.baseUrl + "location/delete/" + name );
+  }
+
+  getLocation(name:String){
+    return this.http.get(this.baseUrl + "location/" + name + "/get");
+  }
+
+  getLocationNamesByCountry(country: String){
+    return this.http.get(this.baseUrl + "location/" + country);
+  }
+
+  getLocationsNames(){
+    return this.http.get(this.baseUrl + "location/locNames");
   }
 }
