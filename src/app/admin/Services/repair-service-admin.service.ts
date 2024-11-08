b18b7b6d63832dcc0f18986e3a3bfe9ca12ffcfb
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-08 16:37:31 +0100

    Created services for admin-repairs

--- src/app/admin/Services/repair-service-admin.service.ts
+++ src/app/admin/Services/repair-service-admin.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Repair} from "../../models/repair";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepairServiceAdminService {
-  baseUrl ="http://localhost:8080/";
+  baseUrl ="http://localhost:8080/repair/";
   constructor(private http: HttpClient) {
-
   }
+
+  addRepair(repair: Repair,location: String){
+    return this.http.post(this.baseUrl + "add?location=" + location, repair);
+  }
+
+  deleteRepair(repair: Repair){
+    return this.http.get(this.baseUrl + "delete");
+  }
+
+  updateRepair(repair: Repair){
+    return this.http.post(this.baseUrl + "update",repair);
+  }
+
+  getRepair(name:String){
+    return this.http.get(this.baseUrl + name + "/get");
+  }
+
+  getLocation(name: String){
+    return this.http.get(this.baseUrl + "location/" + name);
+  }
+
+
 }
