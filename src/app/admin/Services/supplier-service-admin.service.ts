32c4bc9eb91168c3cbab18942be31318403d5e8b
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-08 16:43:11 +0100

    Created services for admin-suppliers

--- src/app/admin/Services/supplier-service-admin.service.ts
+++ src/app/admin/Services/supplier-service-admin.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Supplier} from "../../models/supplier";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupplierServiceAdminService {
-  baseUrl ="http://localhost:8080/";
-  constructor(private http: HttpClient) {
+  baseUrl ="http://localhost:8080/supp/";
 
+  constructor(private http: HttpClient) {}
+
+  addSupplier(supplier: Supplier){
+    return this.http.post("add",supplier);
   }
+
+  updateSupplier(supplier: Supplier){
+    return this.http.put("update",supplier);
+  }
+
+  deleteSupplier(supplier: Supplier){
+    return this.http.post("delete",supplier);
+  }
+
+  getSupplier(name: String){
+    return this.http.get("supplier/" + name);
+  }
+
 }
