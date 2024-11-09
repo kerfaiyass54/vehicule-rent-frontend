a127c91b14e61940e8f76c2dd7a8744eb79063ee
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-09 21:04:57 +0100

    Added things to add new locatons

--- src/app/admin/location-management/add-location/add-location.component.ts
+++ src/app/admin/location-management/add-location/add-location.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {LocationServiceAdminService} from "../../Services/location-service-admin.service";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-add-location',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './add-location.component.html',
   styleUrl: './add-location.component.css'
 })
 export class AddLocationComponent {
+  newLocationForm: FormGroup;
+
+  constructor(private locationService:LocationServiceAdminService, private router: Router, private fb: FormBuilder) {
+    this.newLocationForm = this.fb.group({
+      name: new FormControl(""),
+      country:new FormControl("")
+    });
+  }
+
+  get name(){
+    return this.newLocationForm.get('name');
+  }
+
+  get country(){
+    return this.newLocationForm.get('country');
+  }
+
+  addLocation(){
+    let location : any = {
+      name: this.newLocationForm.value.name,
+      country: this.newLocationForm.value.country,
+    }
+    this.locationService.addLocation(location).subscribe(
+      ()=>{
+        this.router.navigate(['admin/location']);
+      }
+    )
+  }
 
 }
