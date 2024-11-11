ba62bcab01c26b4f70388f750adb60f8023a5c61
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-11 14:34:40 +0100

    Added supplier management interface

--- src/app/admin/supplier-management/supplier-management.component.ts
+++ src/app/admin/supplier-management/supplier-management.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavBarAdminComponent} from "../nav-bar-admin/nav-bar-admin.component";
+import {NgForOf} from "@angular/common";
+import {SupplierServiceAdminService} from "../Services/supplier-service-admin.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-supplier-management',
   standalone: true,
-    imports: [
-        NavBarAdminComponent
-    ],
+  imports: [
+    NavBarAdminComponent,
+    NgForOf
+  ],
   templateUrl: './supplier-management.component.html',
   styleUrl: './supplier-management.component.css'
 })
-export class SupplierManagementComponent {
+export class SupplierManagementComponent implements OnInit{
+  listSuppliers: any[] = [];
 
+  constructor(private supplierservice: SupplierServiceAdminService, private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+
+  goToAddSupplier() {
+
+  }
+
+  goToInfos() {
+
+  }
 }
