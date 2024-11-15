4b6a4ea5e0c4e8616444fc89925627982990df4a
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-15 16:30:04 +0100

    Added deletion

--- src/app/admin/clients-management/clients-info/clients-info.component.ts
+++ src/app/admin/clients-management/clients-info/clients-info.component.ts
@@ -3,13 +3,15 @@ import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {Client} from "../../../models/client";
 import {ClientServiceAdminService} from "../../Services/client-service-admin.service";
 import {LocationServiceAdminService} from "../../Services/location-service-admin.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-clients-info',
   standalone: true,
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './clients-info.component.html',
   styleUrl: './clients-info.component.css'
 })
@@ -17,6 +19,7 @@ export class ClientsInfoComponent implements  OnInit{
   client: Client | any | undefined;
   name :any | null;
   location: Location | any;
+  isModalOpen: boolean = false;
   constructor(private router: Router, private clientService:ClientServiceAdminService, private route: ActivatedRoute, private locationService: LocationServiceAdminService  ) {}
 
   ngOnInit() {
@@ -34,8 +37,20 @@ export class ClientsInfoComponent implements  OnInit{
   }
 
 
+
+
+
   goToUpdate(){
     this.router.navigate(['admin/update',this.name]);
   }
+
+  confirmDelete() {
+    this.clientService.deleteClient(this.client.idClient).subscribe(
+      ()=>{
+        this.router.navigate(['admin/clients']);
+      }
+    )
+  }
+
 
 }
