4dabd3527b40317bf722f2997c20f62919c5d29d
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-16 19:41:52 +0100

    Added location selection to the client

--- src/app/admin/clients-management/add-client/add-client.component.ts
+++ src/app/admin/clients-management/add-client/add-client.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {ClientServiceAdminService} from "../../Services/client-service-admin.service";
 import {Client} from "../../../models/client";
 import {HttpClientModule} from "@angular/common/http";
+import {LocationServiceAdminService} from "../../Services/location-service-admin.service";
 
 @Component({
   selector: 'app-add-client',
@@ -12,21 +13,36 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     ReactiveFormsModule,
     NgIf,
-    RouterLink,HttpClientModule  ],
+    RouterLink, HttpClientModule, NgForOf],
   templateUrl: './add-client.component.html',
   styleUrl: './add-client.component.css'
 })
-export class AddClientComponent {
+export class AddClientComponent implements OnInit{
   newClientForm: FormGroup;
+  locations: any[] = [];
 
 
-  constructor(private fb: FormBuilder, private clientService:ClientServiceAdminService ) {
+  constructor(private fb: FormBuilder, private clientService:ClientServiceAdminService, private locationService: LocationServiceAdminService ) {
+
+
     this.newClientForm = this.fb.group({
       name: new FormControl("",[Validators.required,Validators.pattern("[a-zA-Z ]*")]),
-      cin:new FormControl("",[Validators.required,Validators.pattern("[0-9 ]"),Validators.minLength(8),Validators.maxLength(8)]),
+      cin:new FormControl("",[Validators.required,Validators.pattern("[0-9 ]*"),Validators.minLength(8),Validators.maxLength(8)]),
       mail: new FormControl("",[Validators.required,Validators.email]),
-      password: new FormControl("",[Validators.required,Validators.minLength(6)])
+      password: new FormControl("",[Validators.required,Validators.minLength(6)]),
+      loc: new FormControl("")
     });
+
+
+  }
+
+  ngOnInit() {
+    this.locationService.getAllLocations().subscribe(
+      (res)=>{
+        this.locations = res;
+        console.log(this.locations);
+      }
+    )
   }
 
   get name(){
@@ -45,6 +61,9 @@ export class AddClientComponent {
     return this.newClientForm.get('password');
   }
 
+  get loc(){
+    return this.newClientForm.get('loc');
+  }
 
   addClient() {
     let client : any = {
@@ -54,9 +73,11 @@ export class AddClientComponent {
       email: this.newClientForm.value.mail,
       pass: this.newClientForm.value.password,
       role: "CLIENT",
+
     }
 
-    this.clientService.createClient(client,"Bizerte").subscribe(
+    let location = this.newClientForm.value.loc;
+    this.clientService.createClient(client,location).subscribe(
       ()=>{
         console.log("success")
       }
