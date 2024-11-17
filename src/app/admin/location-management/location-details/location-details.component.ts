bf06ef9c8a414afb42fcd6a28594c31244ee7559
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-17 16:07:36 +0100

    Added infos for supplier and location

--- src/app/admin/location-management/location-details/location-details.component.ts
+++ src/app/admin/location-management/location-details/location-details.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {LocationServiceAdminService} from "../../Services/location-service-admin.service";
 
 @Component({
   selector: 'app-location-details',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './location-details.component.html',
   styleUrl: './location-details.component.css'
 })
-export class LocationDetailsComponent {
+export class LocationDetailsComponent implements OnInit{
 
+   constructor(private locationService:LocationServiceAdminService,private route:ActivatedRoute, private router: Router) {
+}
+
+  location:any;
+  clientsNum: any;
+  repairsNum:any;
+
+  ngOnInit() {
+  }
+
+
+  confirmDelete() {
+
+  }
 }
--- src/app/admin/supplier-management/supplier-infos/supplier-infos.component.ts
+++ src/app/admin/supplier-management/supplier-infos/supplier-infos.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-supplier-infos',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
   templateUrl: './supplier-infos.component.html',
   styleUrl: './supplier-infos.component.css'
 })
 export class SupplierInfosComponent {
 
+  confirmDelete() {
+
+  }
+
+  goToUpdate() {
+
+  }
 }
