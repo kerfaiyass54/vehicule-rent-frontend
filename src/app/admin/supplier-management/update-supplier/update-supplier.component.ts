415af5fc8802154e515decfb8e87ca99bdc99b6a
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-11-18 11:36:13 +0100

    Fixed updating supplier

--- src/app/admin/supplier-management/update-supplier/update-supplier.component.ts
+++ src/app/admin/supplier-management/update-supplier/update-supplier.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgIf} from "@angular/common";
-import {Router, RouterLink} from "@angular/router";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {SupplierServiceAdminService} from "../../Services/supplier-service-admin.service";
 import {ToastrService} from "ngx-toastr";
 import {update} from "@angular-devkit/build-angular/src/tools/esbuild/angular/compilation/parallel-worker";
@@ -20,22 +20,38 @@ import {update} from "@angular-devkit/build-angular/src/tools/esbuild/angular/co
 })
 export class UpdateSupplierComponent implements OnInit{
   newSupplierForm: FormGroup;
+  locations:any[]=[];
+  id:any;
+  supplier: any;
+  idSupp:any;
 
   constructor(private fb: FormBuilder,
               private supplierService:SupplierServiceAdminService,
-              private router: Router,private toastrService:ToastrService ) {
+              private router: Router,private toastrService:ToastrService, private route:ActivatedRoute ) {
     this.newSupplierForm = this.fb.group({
       name: new FormControl("",[Validators.required,Validators.pattern("[a-zA-Z ]*")]),
       nationality:new FormControl("",[Validators.required,Validators.pattern("[a-zA-Z ]*")]),
       mail: new FormControl("",[Validators.required,Validators.email]),
       password: new FormControl("",[Validators.required,Validators.minLength(6)]),
-      loc: new FormControl("")
     });
 
 
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.supplierService.getSupplier(this.id).subscribe(
+      (supp)=>{
+        this.idSupp = supp.idSupp;
+        this.newSupplierForm = this.fb.group({
+          name: new FormControl(supp.suppName,[Validators.required,Validators.pattern("[a-zA-Z ]*")]),
+          nationality:new FormControl(supp.nationality,[Validators.required,Validators.pattern("[a-zA-Z ]*")]),
+          mail: new FormControl(supp.email,[Validators.required,Validators.email]),
+          password: new FormControl(supp.pass,[Validators.required,Validators.minLength(6)]),
+        });
+      }
+    )
+  }
 
   get name(){
     return this.newSupplierForm.get('name');
@@ -56,10 +72,24 @@ export class UpdateSupplierComponent implements OnInit{
 
 
   updateSupplier() {
+    this.supplier = {
+      idSupp:this.idSupp,
+      suppName: this.newSupplierForm.value.name,
+      nationality:this.newSupplierForm.value.nationality,
+      email: this.newSupplierForm.value.mail,
+      pass: this.newSupplierForm.value.password,
+      role: "SUPPLIER",
+    }
 
+    this.supplierService.updateSupplier(this.supplier).subscribe(
+      ()=>{
+        this.toastrService.success("Supplier updated","SUCCESS");
+        this.router.navigate(['admin/infos/supplier',this.newSupplierForm.value.name]);
+      }
+    )
   }
 
   return() {
-
+    this.router.navigate(['admin/infos/supplier',this.id]);
   }
 }
