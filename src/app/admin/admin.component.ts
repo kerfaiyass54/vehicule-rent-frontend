e9575b9ab8da95eedfb726aaf73905da1aa796ed
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-01-18 20:38:55 +0100

    Added a footer

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {NavBarAdminComponent} from "./nav-bar-admin/nav-bar-admin.component";
+import {FooterAppComponent} from "../footer-app/footer-app.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-    imports: [
-        RouterOutlet,
-        NavBarAdminComponent,
-    ],
+  imports: [
+    RouterOutlet,
+    NavBarAdminComponent,
+    FooterAppComponent,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
