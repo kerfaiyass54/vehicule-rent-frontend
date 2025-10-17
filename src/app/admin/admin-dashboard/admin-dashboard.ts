663cbf111e7a32ea1367b57317a7aa54e17a9a60
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-10-17 20:09:19 +0100

    Added dashboard and fixed standalone

--- src/app/admin/admin-dashboard/admin-dashboard.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
+  standalone: true,
   imports: [],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css'
