86667c6b830a2effaec8489fda783c90c692123f
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-22 16:16:02 +0100

    Added services for user management

---
+++ src/app/user-management/services/user-manage.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserManage {
+
+  baseUrlKeycloak = "http://localhost:8100/keycloak/"
+
+  constructor(private http:HttpClient) {}
+
+
+  createUser(user: any){
+    return this.http.post(this.baseUrlKeycloak, user);
+  }
+
+  getUsers(){
+    return this.http.get(this.baseUrlKeycloak + "users");
+  }
+
+  deleteUser(id: any){
+    return this.http.delete(this.baseUrlKeycloak + "user/" + id)
+  }
+
+  updatePassword(id: any, password: any){
+    return this.http.put(this.baseUrlKeycloak + "user/" + id + "/password", password);
+  }
+
+  getRoles(){
+    return this.http.get(this.baseUrlKeycloak + "roles");
+  }
+
+  updateUser(userId: any, updateUser: any){
+    return this.http.put(this.baseUrlKeycloak + "user/" + userId + "/update", updateUser);
+  }
+
+
+
+}
