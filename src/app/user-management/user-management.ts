6072579c014ffbc7dbb8199c0c1fd86c27062843
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-22 16:44:06 +0100

    Added login history

--- src/app/user-management/user-management.ts
+++ src/app/user-management/user-management.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
 import {AccountInfo} from "./account-info/account-info";
+import {LoginHistory} from "./login-history/login-history";
 
 @Component({
   selector: 'app-user-management',
   imports: [
     MatTabGroup,
     MatTab,
-    AccountInfo
+    AccountInfo,
+    LoginHistory
   ],
   standalone: true,
   templateUrl: './user-management.html',
