7044638d80b0e80cf8bd6250d99053979c0a17ea
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-29 21:37:00 +0100

    Edited some css styles for details

--- src/app/user-management/update-user/update-user.ts
+++ src/app/user-management/update-user/update-user.ts
@@ -3,8 +3,10 @@ import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder} from "@angular/forms";
 import { MatDialogRef } from '@angular/material/dialog';
+import {KeycloakService} from "../../shared/keycloak.service";
+import {UserManage} from "../services/user-manage";
 
 @Component({
   selector: 'app-update-user',
@@ -25,7 +27,32 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class UpdateUser {
 
-  constructor(private dialogRef: MatDialogRef<UpdateUser>) {}
+  userForm: FormGroup;
+  userInfo?: any;
+  emails = [];
+  users = [];
+
+
+  constructor(private dialogRef: MatDialogRef<UpdateUser>,private fb: FormBuilder,
+              private keycloakService: KeycloakService, private userManager: UserManage) {
+
+    this.userInfo = this.keycloakService.loadUserProfile();
+    this.userForm = this.fb.group({
+      firstName: new FormControl(this.userInfo.firstName, [Validators.required]),
+      email: new FormControl(this.userInfo.email, [Validators.required, Validators.email]),
+      lastName: new FormControl(this.userInfo.lastName, [Validators.required])
+    });
+
+
+
+
+
+  }
+
+
+  onSubmit(){
+
+  }
 
 
 }
