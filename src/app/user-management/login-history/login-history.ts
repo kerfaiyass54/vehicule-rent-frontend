ed4010ceea4fbc733fe1b5fd9d63c006958a8528
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-13 22:24:53 +0100

    Added UI styles to make it visible and beautiful

--- src/app/user-management/login-history/login-history.ts
+++ src/app/user-management/login-history/login-history.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatNoDataRow, MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from "@angular/material/table";
+import {MatInput} from "@angular/material/input";
+import {MatPaginator} from "@angular/material/paginator";
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from "@angular/cdk/drag-drop";
+import {MatSort, MatSortHeader, Sort} from "@angular/material/sort";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
 
 @Component({
   selector: 'app-login-history',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatTable,
+    MatInput,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatNoDataRow,
+    MatPaginator,
+    CdkDrag,
+    CdkDropList,
+    MatSort,
+    MatSortHeader
+  ],
   standalone: true,
   templateUrl: './login-history.html',
   styleUrl: './login-history.css'
 })
-export class LoginHistory {
+export class LoginHistory implements OnInit, AfterViewInit{
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  sessions: any[] = [];
+  dataSource = new MatTableDataSource(this.sessions);
+  private _liveAnnouncer = inject(LiveAnnouncer);
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.displayedColumns, event.previousIndex, event.currentIndex);
+  }
+
+
+
 
 }
