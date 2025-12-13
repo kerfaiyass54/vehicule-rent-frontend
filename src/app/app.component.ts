56dc88b06b55d7e30f0382f406e17141d608303b
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-13 21:35:44 +0100

    Added session saving to the index while logging out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from "@angular/common/http";
+import {getKeycloak} from "./shared/keycloak-init";
+import {SessionService} from "./shared/session-service";
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,33 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private sessionService: SessionService) {
+  }
+
   title = 'vehicule-rent-front';
+  private isRefreshing = false;
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (
+      event.key === 'F5' ||
+      (event.ctrlKey && event.key.toLowerCase() === 'r')
+    ) {
+      this.isRefreshing = true;
+    }
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  onAppLeave(event: BeforeUnloadEvent) {
+    if (this.isRefreshing) {
+      return;
+    }
+    this.sessionService.saveSession(getKeycloak().profile?.username, getKeycloak().profile?.email);
+    sessionStorage.clear();
+    localStorage.removeItem('token');
+  }
+
+
 
 }
