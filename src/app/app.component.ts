78f5714bf18624cf55695b67042ba9e1b2e2aeeb
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-14 21:53:44 +0100

    Removed all the events of appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component,HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule} from "@angular/common/http";
-import {getKeycloak} from "./shared/keycloak-init";
-import {SessionService} from "./shared/session-service";
 
 @Component({
   selector: 'app-root',
@@ -13,31 +11,8 @@ import {SessionService} from "./shared/session-service";
 })
 export class AppComponent {
 
-  constructor(private sessionService: SessionService) {
-  }
-
   title = 'vehicule-rent-front';
-  private isRefreshing = false;
 
-  @HostListener('window:keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent) {
-    if (
-      event.key === 'F5' ||
-      (event.ctrlKey && event.key.toLowerCase() === 'r')
-    ) {
-      this.isRefreshing = true;
-    }
-  }
-
-  @HostListener('window:beforeunload', ['$event'])
-  onAppLeave(event: BeforeUnloadEvent) {
-    if (this.isRefreshing) {
-      return;
-    }
-    this.sessionService.saveSession(getKeycloak().profile?.username, getKeycloak().profile?.email);
-    sessionStorage.clear();
-    localStorage.removeItem('token');
-  }
 
 
 
