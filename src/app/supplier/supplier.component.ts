c1abf3e4e64588c5c42f8c8606709e86a2cc0198
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-17 16:32:45 +0100

    Added navgation bar component UI to suppliers

--- src/app/supplier/supplier.component.ts
+++ src/app/supplier/supplier.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {NavBar} from "../UIs/nav-bar/nav-bar";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-supplier',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBar,
+    RouterOutlet
+  ],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.css'
 })
 export class SupplierComponent {
+
+  mainRoute: any = '/supplier';
+  list: any[] = [
+    {route: '', name: 'Vehicules', drop: true, sublinks : [
+        {link: '/supplier/demands', name: 'Demands'},
+        {link: '/supplier/categories', name: 'Categories'},
+        {link: '/supplier/vehicules', name: 'Vehicules'},
+      ]},
+    {route: '/supplier/adresses', name: 'Adresses', drop: false, sublinks :[
+      ]},
+    {route: '/supplier/subscriptions', name: 'Subscriptions', drop: false, sublinks : [
+      ]},
+  ]
 
 }
