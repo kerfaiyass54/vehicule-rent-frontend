ff80c8adcca1a31472e46544338f8fcbfff63d6f
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-17 16:22:47 +0100

    Added navgation bar component UI to repairs

--- src/app/repair/repair.component.ts
+++ src/app/repair/repair.component.ts
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class RepairComponent {
 
+  mainRoute: any = '/repair';
+  list: any[] = [
+    {route: '', name: 'Vehicules', drop: true, sublinks : [
+        {link: '/repair/ticket', name: 'Tickets'},
+        {link: '/repair/demand', name: 'Demands'},
+        {link: '/repair/veh-repair', name: 'Repairs'},
+      ]}
+  ]
+
+
 }
